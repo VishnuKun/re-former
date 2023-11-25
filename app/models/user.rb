8c55c6b657fc190e38b35de98f674251bed67c84
Vishnu <vishnukun954AV@protonmail.com>
2023-11-25 12:10:21 +0545

    Added password_digest col

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # User model
 class User < ApplicationRecord
-  validates :username, :email, :password, presence: true
   has_secure_password
+  validates :username, :email, :password, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_25_050529) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_25_062359) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
